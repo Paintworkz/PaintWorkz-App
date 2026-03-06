fbe8e95b7a24ca88eab0618717c149d279b6e206
Paintworkz <cjbed@icloud.com>
2026-03-07 10:02:18 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'paintworkz-v1.8.14';
+const CACHE_NAME = 'paintworkz-v1.8.16';
 const ASSETS = ['./', './index.html', './logo.png', './manifest.json'];
 
 self.addEventListener('install', e => {
