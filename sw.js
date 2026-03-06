561f531061dc41b973f7b9655d7bf16d9f141052
Paintworkz <cjbed@icloud.com>
2026-03-07 09:35:50 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,22 @@
-const CACHE_NAME = 'pw-cloud-v1';
+const CACHE_NAME = 'paintworkz-v1.8.14';
 const ASSETS = ['./', './index.html', './logo.png', './manifest.json'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
